fbe2f2659fe9b388de67abaf7c146d72dbff670f
Ruben Daniels <ruben@c9.io>
2013-12-23 11:47:41 -0800

    fixes oops in welcome

--- welcome.js
+++ welcome.js
@@ -247,7 +247,7 @@ define(function(require, exports, module) {
                 }
                 
                 layout.on("themeChange", setTheme);
-                setTheme({ theme: settings.get("user/general/@theme") || "dark" });
+                setTheme({ theme: settings.get("user/general/@skin") || "dark" });
                 
                 doc.title = "Welcome", 
                 doc.meta.welcome = true;
